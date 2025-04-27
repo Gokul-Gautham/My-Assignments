f955ab42a167bfd510422fe6a2c3bb7025189218
Gokul-Gautham <48408280+Gokul-Gautham@users.noreply.github.com>
2025-04-27 13:55:40 +0530

    Add files via upload
    Day 1 - Activities & Assignments

---
+++ day1/Chrome.java
@@ -0,0 +1,24 @@
+//Home Assignment 2
+
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float value = 91.0f ;
+		String name = "Google" ;
+		boolean status = false ;
+		int year = 2008 ;
+		char key = 'C' ;
+
+		System.out.println("Float : " + value);
+		System.out.println("String : " + name);
+		System.out.println("boolean : " + status);
+		System.out.println("Int : " + year);
+		System.out.println("Char : " + key);
+
+
+	}
+
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class FirstProgram {
+	
+	//Shortcut type "main" ctrl+space
+	public static void main(String[] args) {
+		
+		//Shortcut type "Syso" ctrl+space
+		System.out.println("Hello ");
+		
+		//Shortcut for execution ctrl+fn+f11
+	}
+
+}
